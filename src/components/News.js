6418bd6fc2ffe362363ad9526b29fa0e1775f51a
Sümeyye <sumeyyeaydemir@gmail.com>
2021-07-28 18:12:27 +0300

    add component

---
+++ src/components/News.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NewsItem from './NewsItem';
+
+class News extends React.Component{
+    render(){
+        return(
+            <div>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+            </div>
+        )
+    }
+
+}
+
+export default News;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,16 @@
+import React,  { Component } from 'react';
+import '../NewsItem.css';
+
+
+class NewsItem extends Component{
+    render(){
+        return(
+            <div>
+                <h1 className="title">Hello React</h1>
+                <p>Hello Hello Hello</p>
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
